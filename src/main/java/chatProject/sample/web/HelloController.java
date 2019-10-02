dbf994fded6c99bdcdb617619f6d1c51a5c6da5d
wonjun <94_wonjun@naver.com>
2019-10-02 15:54:48 +0900

    최초 등록

---
+++ src/main/java/chatProject/sample/web/HelloController.java
@@ -0,0 +1,14 @@
+package chatProject.sample.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	
+	@RequestMapping("/")
+	public String hello(){
+		return "hello";
+	}
+
+}
